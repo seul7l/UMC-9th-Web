3cbab067b0a7255b4c12b3cdc315af027ee8dfbb
Seulgi Han <chiaki2c@skuniv.ac.kr>
2025-09-26 21:05:37 +0900

    todolist.tsx로 옮김

--- Week02/ReactViteTS/src/main.tsx
+++ Week02/ReactViteTS/src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { CounterProvider } from "./context/CounterProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <CounterProvider>
-      <App />
-    </CounterProvider>
+    <App />
   </StrictMode>
 );
