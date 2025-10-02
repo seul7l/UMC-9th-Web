dac0fb821cf444de45ae273f92f5121e4c34bf3f
Seulgi Han <chiaki2c@skuniv.ac.kr>
2025-10-03 00:23:03 +0900

    1주차 누락 파일 푸시

---
+++ Week01/TypeScript.ts
@@ -0,0 +1,39 @@
+{
+  /*}
+const isString: string = "name";
+const isString2: string = 123;
+
+const isNumber: number = 1;
+const isNumber2: number = "hello";
+
+const isBoolean: boolean = true;
+const isBoolean2: boolean = "red";
+
+const isNull: null = null;
+const isNull2: null = 0;
+
+const isUndefined: undefined = undefined;
+const isUndefined2: undefined = null;
+const isUndefined3: undefined = 456;
+
+const isSymbol: symbol = Symbol("심볼임");
+const isSymbol2: symbol = Symbol("심볼임");
+const isSymbol3: symbol = new Symbol("심볼임");
+
+console.log(isSymbol === isSymbol2); // false
+
+const isBigint: bigint = 123456789123456789n;
+const isBigint2: bigint = BigInt(123456789123456789);
+const isBigint3: bigint = 10000000000;
+
+const isObject: { name: string; age: number } = {
+  name: "홍길동",
+  age: 20,
+};
+const isObject2: { name: string; age: number } = {
+  name: "홍길동",
+};
+
+
+*/
+}
